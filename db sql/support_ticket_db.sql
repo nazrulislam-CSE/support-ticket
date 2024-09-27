e56af5421b1503248f180f4702c1fded038985ed
Nazrul Islam <101917400+nazrulislam-CSE@users.noreply.github.com>
2024-09-27 20:50:00 +0600

    Update and rename b_hisab.sql to support_ticket_db.sql

--- db sql/support_ticket_db.sql
+++ db sql/support_ticket_db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 28, 2023 at 06:31 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.1.17
+-- Generation Time: Sep 27, 2024 at 04:46 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,8 +18,20 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `b_hisab`
+-- Database: `support_ticket_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -47,15 +59,15 @@ CREATE TABLE `admins` (
 --
 
 INSERT INTO `admins` (`id`, `name`, `username`, `email`, `phone`, `address`, `email_verified_at`, `password`, `show_password`, `status`, `created_at`, `updated_at`) VALUES
-(1, 'Admin', 'admin', 'admin@gmail.com', 1950594285, NULL, '2023-08-04 22:35:52', '$2y$10$nYiL2SlNfVjfguWp5c2ope1mp0SGJ.ypbM9eb5uUvgvutIW.NOwGm', 'admin', 1, '2023-08-04 22:35:52', '2023-08-04 22:35:52');
+(1, 'Admin', 'admin', 'admin@gmail.com', 1950594285, NULL, '2024-09-27 05:16:34', '$2y$10$je94ctXixrhuVUnNvfo4E.KtWpPDri88emQ.91GmiFb/8V/Fp/ihm', 'admin', 1, '2024-09-27 05:16:34', '2024-09-27 05:16:34');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `clients`
+-- Table structure for table `expenses`
 --
 
-CREATE TABLE `clients` (
+CREATE TABLE `expenses` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -75,21 +87,6 @@ CREATE TABLE `failed_jobs` (
   `payload` longtext NOT NULL,
   `exception` longtext NOT NULL,
   `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `groups`
---
-
-CREATE TABLE `groups` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `created_by` varchar(255) DEFAULT NULL,
-  `status` int(11) DEFAULT 1,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -116,8 +113,12 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (5, '2019_12_14_000001_create_personal_access_tokens_table', 1),
 (6, '2023_08_01_054611_create_admins_table', 1),
 (7, '2023_08_01_055426_create_permission_tables', 1),
-(8, '2023_08_06_062805_create_clients_table', 2),
-(9, '2023_08_06_084431_create_groups_table', 2);
+(8, '2023_08_06_180644_create_accounts_table', 1),
+(9, '2023_08_07_003206_create_receives_table', 1),
+(10, '2023_08_07_003353_create_suppliers_table', 1),
+(11, '2023_08_07_040801_create_expenses_table', 1),
+(12, '2024_09_27_122556_create_tickets_table', 2),
+(13, '2024_09_27_122715_create_responses_table', 2);
 
 -- --------------------------------------------------------
 
@@ -193,8 +194,8 @@ CREATE TABLE `permissions` (
 --
 
 INSERT INTO `permissions` (`id`, `name`, `guard_name`, `created_at`, `updated_at`) VALUES
-(1, 'access-all', 'web', '2023-08-04 22:35:52', '2023-08-04 22:35:52'),
-(2, 'settings', 'web', '2023-08-04 22:35:52', '2023-08-04 22:35:52');
+(1, 'access-all', 'web', '2024-09-27 05:16:34', '2024-09-27 05:16:34'),
+(2, 'settings', 'web', '2024-09-27 05:16:34', '2024-09-27 05:16:34');
 
 -- --------------------------------------------------------
 
@@ -218,6 +219,33 @@ CREATE TABLE `personal_access_tokens` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `receives`
+--
+
+CREATE TABLE `receives` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `responses`
+--
+
+CREATE TABLE `responses` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `ticket_id` bigint(20) UNSIGNED NOT NULL,
+  `admin_id` bigint(20) UNSIGNED NOT NULL,
+  `response` text NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `roles`
 --
 
@@ -234,7 +262,7 @@ CREATE TABLE `roles` (
 --
 
 INSERT INTO `roles` (`id`, `name`, `guard_name`, `created_at`, `updated_at`) VALUES
-(1, 'Superuser', 'web', '2023-08-04 22:35:52', '2023-08-04 22:35:52');
+(1, 'Superuser', 'web', '2024-09-27 05:16:34', '2024-09-27 05:16:34');
 
 -- --------------------------------------------------------
 
@@ -257,6 +285,49 @@ INSERT INTO `role_has_permissions` (`permission_id`, `role_id`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `suppliers`
+--
+
+CREATE TABLE `suppliers` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tickets`
+--
+
+CREATE TABLE `tickets` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `status` enum('open','in_progress','resolved','closed') NOT NULL DEFAULT 'open',
+  `priority` enum('low','medium','high') NOT NULL DEFAULT 'medium',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `tickets`
+--
+
+INSERT INTO `tickets` (`id`, `user_id`, `subject`, `description`, `status`, `priority`, `created_at`, `updated_at`) VALUES
+(10, 1, 'Voluptate ipsum volu', 'Exercitation harum m', 'open', 'low', '2024-09-27 07:30:51', '2024-09-27 08:17:58'),
+(11, 1, 'Corrupti est in sin', 'Non reprehenderit su', 'open', 'high', '2024-09-27 07:39:59', '2024-09-27 07:39:59'),
+(12, 1, 'Corrupti est in sin', 'Non reprehenderit su', 'open', 'high', '2024-09-27 07:40:43', '2024-09-27 07:40:43'),
+(13, 1, 'Corrupti est in sin', 'Non reprehenderit su', 'open', 'high', '2024-09-27 07:42:14', '2024-09-27 07:42:14'),
+(14, 1, 'Corrupti est in sin', 'Non reprehenderit su', 'closed', 'high', '2024-09-27 07:43:59', '2024-09-27 08:22:28'),
+(15, 1, 'Corrupti est in sin', 'Non reprehenderit su', 'closed', 'high', '2024-09-27 07:44:39', '2024-09-27 08:21:23'),
+(16, 1, 'Corrupti est in sin', 'Non reprehenderit su', 'closed', 'high', '2024-09-27 07:50:03', '2024-09-27 08:19:20'),
+(17, 1, 'Deleniti facilis off', 'Totam reiciendis dol', 'open', 'low', '2024-09-27 08:23:09', '2024-09-27 08:23:09');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -265,17 +336,37 @@ CREATE TABLE `users` (
   `name` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
+  `phone` bigint(20) UNSIGNED DEFAULT NULL,
   `image` varchar(255) DEFAULT NULL,
-  `show_password` varchar(255) DEFAULT NULL,
+  `fathers_name` varchar(255) DEFAULT NULL,
+  `mothers_name` varchar(255) DEFAULT NULL,
+  `present_address` varchar(255) DEFAULT NULL,
+  `parmanent_address` varchar(255) DEFAULT NULL,
+  `date_of_birth` varchar(255) DEFAULT NULL,
+  `nationality` varchar(255) DEFAULT NULL,
+  `religion` varchar(255) DEFAULT NULL,
+  `nid` varchar(255) DEFAULT NULL,
+  `birth_certificate` varchar(255) DEFAULT NULL,
+  `blood_group` varchar(255) DEFAULT NULL,
+  `gender` varchar(255) DEFAULT NULL,
+  `edu_qualification` varchar(255) DEFAULT NULL,
+  `experience` varchar(255) DEFAULT NULL,
+  `staff_id` varchar(255) DEFAULT NULL,
+  `staff_type` varchar(255) DEFAULT NULL,
+  `department_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `designation_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `office_zone` varchar(255) DEFAULT NULL,
+  `joining_date` varchar(255) DEFAULT NULL,
+  `discharge_date` varchar(255) DEFAULT NULL,
+  `machine_id` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `marital_status` varchar(255) DEFAULT NULL,
+  `show_password` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
-  `phone` bigint(20) UNSIGNED NOT NULL,
-  `address` varchar(255) DEFAULT NULL,
-  `country_name` varchar(255) NOT NULL DEFAULT 'Bangladesh',
-  `country_code` varchar(255) NOT NULL DEFAULT '+880',
-  `code` varchar(255) DEFAULT 'BD',
-  `status` tinyint(4) NOT NULL DEFAULT 0,
   `created_by` varchar(255) DEFAULT NULL,
+  `updated_by` varchar(255) DEFAULT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT 0,
   `is_deleted` tinyint(4) NOT NULL DEFAULT 0,
   `menu` tinyint(4) NOT NULL DEFAULT 1,
   `remember_token` varchar(100) DEFAULT NULL,
@@ -287,14 +378,19 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `username`, `email`, `image`, `show_password`, `password`, `email_verified_at`, `phone`, `address`, `country_name`, `country_code`, `code`, `status`, `created_by`, `is_deleted`, `menu`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'user', 'Hossain', 'user@gmail.com', NULL, '12345678', '$2y$10$SSmFIJvXBcfZFnkJSb9cM.pL7uEkcsIUtmdYEVab0twDE.U2f2xAG', NULL, 1721523654, NULL, 'Bangladesh', '+880', 'BD', 0, 'Hossain', 0, 1, NULL, '2023-08-03 05:35:09', '2023-08-03 05:35:09'),
-(2, 'Demo User', 'demo', 'demo@gmail.com', NULL, 'demo1234', '$2y$10$zIoH3rM8IdODNeTNkgRqluS0kmXS5PYQ6ErMQsHUvehshfMABlCEq', '2023-08-04 22:35:52', 1950594285, NULL, 'Bangladesh', '+880', 'BD', 1, 'demo', 0, 1, NULL, '2023-08-04 22:35:52', '2023-08-04 22:35:52'),
-(3, 'Demo Staff', 'demo-staff', 'demostaff@gmail.com', NULL, 'demo1234', '$2y$10$DWDtpMnnU0q77UZ0m22vM.RMQWoWhAz3qFVWldr3LHced611mHUgS', '2023-08-04 22:35:52', 1950594280, NULL, 'Bangladesh', '+880', 'BD', 1, 'demo', 0, 1, NULL, '2023-08-04 22:35:52', '2023-08-04 22:35:52');
+INSERT INTO `users` (`id`, `name`, `username`, `email`, `phone`, `image`, `fathers_name`, `mothers_name`, `present_address`, `parmanent_address`, `date_of_birth`, `nationality`, `religion`, `nid`, `birth_certificate`, `blood_group`, `gender`, `edu_qualification`, `experience`, `staff_id`, `staff_type`, `department_id`, `designation_id`, `office_zone`, `joining_date`, `discharge_date`, `machine_id`, `description`, `marital_status`, `show_password`, `password`, `email_verified_at`, `created_by`, `updated_by`, `status`, `is_deleted`, `menu`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Demo User', 'demo', 'demo@gmail.com', 1950594285, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'demo1234', '$2y$10$Uyf2HgiqfjJrTjQBpgTNUe2YFkg8l9lNMQGor.QzXRwuETVM8wVhW', '2024-09-27 05:16:34', 'demo', NULL, 1, 0, 1, NULL, '2024-09-27 05:16:34', '2024-09-27 05:16:34'),
+(2, 'Demo Staff', 'demo-staff', 'demostaff@gmail.com', 1950594280, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'demo1234', '$2y$10$0GMRpXGi8ff4mLigtikwnuf.SH6Zc61AeJILpvAYGpdisapMeckUe', '2024-09-27 05:16:34', 'demo', NULL, 1, 0, 1, NULL, '2024-09-27 05:16:34', '2024-09-27 05:16:34');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `admins`
@@ -305,9 +401,9 @@ ALTER TABLE `admins`
   ADD UNIQUE KEY `admins_email_unique` (`email`);
 
 --
--- Indexes for table `clients`
+-- Indexes for table `expenses`
 --
-ALTER TABLE `clients`
+ALTER TABLE `expenses`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -316,12 +412,6 @@ ALTER TABLE `clients`
 ALTER TABLE `failed_jobs`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
-
---
--- Indexes for table `groups`
---
-ALTER TABLE `groups`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `migrations`
@@ -371,6 +461,20 @@ ALTER TABLE `personal_access_tokens`
   ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
 
 --
+-- Indexes for table `receives`
+--
+ALTER TABLE `receives`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `responses`
+--
+ALTER TABLE `responses`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `responses_ticket_id_foreign` (`ticket_id`),
+  ADD KEY `responses_admin_id_foreign` (`admin_id`);
+
+--
 -- Indexes for table `roles`
 --
 ALTER TABLE `roles`
@@ -385,6 +489,19 @@ ALTER TABLE `role_has_permissions`
   ADD KEY `role_has_permissions_role_id_foreign` (`role_id`);
 
 --
+-- Indexes for table `suppliers`
+--
+ALTER TABLE `suppliers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tickets`
+--
+ALTER TABLE `tickets`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `tickets_user_id_foreign` (`user_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -397,15 +514,21 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `admins`
 --
 ALTER TABLE `admins`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `clients`
+-- AUTO_INCREMENT for table `expenses`
 --
-ALTER TABLE `clients`
+ALTER TABLE `expenses`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
@@ -415,16 +538,10 @@ ALTER TABLE `failed_jobs`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `groups`
---
-ALTER TABLE `groups`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `permissions`
@@ -439,16 +556,40 @@ ALTER TABLE `personal_access_tokens`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `receives`
+--
+ALTER TABLE `receives`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `responses`
+--
+ALTER TABLE `responses`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `roles`
 --
 ALTER TABLE `roles`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `suppliers`
+--
+ALTER TABLE `suppliers`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tickets`
+--
+ALTER TABLE `tickets`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
@@ -467,11 +608,24 @@ ALTER TABLE `model_has_roles`
   ADD CONSTRAINT `model_has_roles_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE;
 
 --
+-- Constraints for table `responses`
+--
+ALTER TABLE `responses`
+  ADD CONSTRAINT `responses_admin_id_foreign` FOREIGN KEY (`admin_id`) REFERENCES `admins` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `responses_ticket_id_foreign` FOREIGN KEY (`ticket_id`) REFERENCES `tickets` (`id`) ON DELETE CASCADE;
+
+--
 -- Constraints for table `role_has_permissions`
 --
 ALTER TABLE `role_has_permissions`
   ADD CONSTRAINT `role_has_permissions_permission_id_foreign` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `role_has_permissions_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `tickets`
+--
+ALTER TABLE `tickets`
+  ADD CONSTRAINT `tickets_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
